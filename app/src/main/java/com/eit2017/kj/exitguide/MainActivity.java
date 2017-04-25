7bf463f6253fbe85f437cdcab2d1777e68385cc4
Kamila <tkamilat@gmail.com>
2017-04-25 20:43:07 +0200

    compass init

--- app/src/main/java/com/eit2017/kj/exitguide/MainActivity.java
+++ app/src/main/java/com/eit2017/kj/exitguide/MainActivity.java
@@ -1,13 +1,55 @@
 package com.eit2017.kj.exitguide;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    TextView tvDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDirection = (TextView) findViewById(R.id.tvDirection);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // to stop the listener and save battery - K: do we need this?
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get the angle around the z-axis rotated:
+        // 0 - North, 90 - East, 180 - South, 270 - West
+        float degree = Math.round(event.values[0]);
+        tvDirection.setText(Float.toString(degree));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //not in use
     }
 }
