dac476a6dc6b99f6af8f7513cbb7912f0ed289e5
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-04-30 18:09:54 +0200

    Speech recognition (only english so far)

--- app/src/main/java/com/eit2017/kj/exitguide/MainActivity.java
+++ app/src/main/java/com/eit2017/kj/exitguide/MainActivity.java
@@ -1,18 +1,31 @@
 package com.eit2017.kj.exitguide;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     TextView tvDirection;
+    private TextView textSpoken;
+    private Button buttonSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +34,65 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         tvDirection = (TextView) findViewById(R.id.tvDirection);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //SpeechRecognition
+        buttonSpeak = (Button) findViewById(R.id.buttonSpeak);
+        textSpoken = (TextView) findViewById(R.id.textSpoken);
+
+        buttonSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speechInput();
+            }
+        });
     }
 
+    private void speechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "pl-PL");
+        intent.putExtra("android.speech.extra.EXTRA_ADDITIONAL_LANGUAGES", new String[]{"pl"});
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textSpoken.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+*/
     @Override
     protected void onResume() {
         super.onResume();
